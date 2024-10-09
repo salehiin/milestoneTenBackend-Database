e2ee9f1ff5f0272ece88993fc529e354f922e8b8
salehiin <salehin.ay@gmail.com>
2024-10-09 14:31:24 +0600

    v55.3-6 data send to client side to serverside, serverside to database, data show in backend & frontend, CRD 0f CRUD

---
+++ m2/simple-crud-client/src/components/Users.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+
+const Users = () => {
+
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers);
+
+    const handleDelete = _id =>{
+        console.log('Delete', _id);
+        fetch(`http://localhost:5000/users/${_id}`, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0){
+                alert('Deleted successfully');
+                const remaining = users.filter(user => user._id !== _id);
+                setUsers(remaining);
+            }
+        })
+
+    }
+
+    return (
+        <div>
+            <h2>Users: {users.length}</h2>
+            <div>
+                {
+                    users.map(user =><p key={user._id}>{user.name} : {user.email}
+                    <button
+                        onClick={()=>handleDelete(user._id)}
+                    >X</button></p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
--- m2/simple-crud-server/index.js
+++ m2/simple-crud-server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -27,6 +27,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // const database = client.db("usersDB");
+    // const userCollection = database.collection("users");
+
+    const userCollection = client.db("usersDB").collection("users");
+
+    app.get('/users', async(req, res) => {
+      const cursor = userCollection.find()
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post('/users', async(req, res) =>{
+        const user = req.body;
+        console.log('New user', user);
+        const result = await userCollection.insertOne(user);
+        res.send(result);
+    });
+
+    app.delete('/users/:id', async(req, res) =>{
+      const id = req.params.id;
+      console.log('please delete from database', id);
+      const query = {_id: new ObjectId(id)}
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
